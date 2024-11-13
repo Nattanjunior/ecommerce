441dc332454bf5663f02df223fe1978ae496a8be
nattanjunior <fnatanieljunior@gmail.com>
2024-11-13 19:28:08 -0300

    modified, deleted headers

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,15 +7,9 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
     await initMongoose();
     const products = await Product.find().exec();
 
-    return new Response(JSON.stringify(products), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
+    return new Response(JSON.stringify(products), { status: 200, });
   } catch (error) {
     console.error("Erro ao obter produtos:", error);
-    return new Response(JSON.stringify({ message: "Erro ao obter produtos" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    return new Response(JSON.stringify({ message: "Erro ao obter produtos" }), { status: 500, });
   }
 }
