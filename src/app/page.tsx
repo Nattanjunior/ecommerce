9bdf4223c9e30915bc372ee1da59198a1cd84208
nattanjunior <fnatanieljunior@gmail.com>
2024-11-13 19:27:24 -0300

    call api, render data api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,42 @@
 
 import Image from "next/image";
 import { useEffect, useState } from 'react'
+import type { SchemaProps } from "../../models/products";
+import Product from "../../components/products";
 
 
 export default function Home() {
-  const [productsInfo, setproductsInfo] = useState([])
+  const [productsInfo, setProductsInfo] = useState<SchemaProps[]>([])
   useEffect(() => {
     fetch('/api/products')
       .then(response => response.json())
-      .then(data => setproductsInfo(data))
+      .then(data => setProductsInfo(data))
   }, [])
 
-  const categoriesName = productsInfo.map(p => {
-
-  })
+  const categoriesName = [...new Set(productsInfo.map((p) => p.category))]
 
   return (
     <div className="p-5">
       <section>
-        <h2 className="text-2xl">Mobiles</h2>
-        <div className="py-4">
-          <div className="w-64">
-            <div className="bg-blue-100 p-5 rounded-xl">
-              <Image src='/products/iphone.png' alt=""
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div className="mt-2">
-              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
-              <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, laborum voluptates quis recusandae repellat ullam fugiat, beatae voluptate illo dicta mollitia? </p>
-            </div>
-            <div className="flex mt-1">
-              <p className="text-2xl font-bold grow">$899</p>
-              <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
+        {categoriesName.map(categoryName => (
+          <div key={categoryName}>
+            <h2 className="text-2xl capitalize">{categoryName}</h2>
+            <div className="flex -mx-5 overflow-x-scroll">
+              {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
+                <div key={productInfo.id} className="px-5">
+                  <Product
+                    name={productInfo.name}
+                    price={productInfo.price}
+                    description={productInfo.description}
+                    picture={productInfo.picture}
+                  />
+                </div>
+              ))}
             </div>
           </div>
-        </div>
+        ))}
+        {/* <div className="py-4">
+        </div> */}
       </section>
     </div>
   );
