ea442970074e76f11c6ab4040f0ad9449ae44980
nattanjunior <fnatanieljunior@gmail.com>
2024-11-15 01:48:53 -0300

    created by function findAllProducts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,10 +2,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { initMongoose } from "../../../../lib/mongoose";
 import Product from "../../../../models/products";
 
+
+export default async function findAllProducts() {
+  return Product.find().exec()
+}
+
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
+
   try {
     await initMongoose();
-    const products = await Product.find().exec();
+    const products = await findAllProducts();
 
     return new Response(JSON.stringify(products), { status: 200, });
   } catch (error) {
