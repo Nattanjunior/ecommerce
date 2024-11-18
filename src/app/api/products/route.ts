75a150ad130a1b80064d64aa14ce74648fe95af7
nattanjunior <fnatanieljunior@gmail.com>
2024-11-18 16:51:47 -0300

    retornando produtos especificos selecionados

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,4 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { initMongoose } from "../../../../lib/mongoose";
 import Product from "../../../../models/products";
 
@@ -7,13 +6,22 @@ export default async function findAllProducts() {
   return Product.find().exec()
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request) {
 
   try {
     await initMongoose();
-    const products = await findAllProducts();
 
-    return new Response(JSON.stringify(products), { status: 200, });
+    const url = new URL(req.url)
+    const ids = url.searchParams.get("ids")
+    if (ids) {
+      const idsArray = ids.split(',');
+      const products = await Product.find({ '_id': { $in: idsArray } }).exec()
+      return new Response(JSON.stringify(products), { status: 200 })
+    } else {
+      const products = await findAllProducts();
+      return new Response(JSON.stringify(products), { status: 200, });
+    }
+
   } catch (error) {
     console.error("Erro ao obter produtos:", error);
     return new Response(JSON.stringify({ message: "Erro ao obter produtos" }), { status: 500, });
