51fc8a6f259b03cb39c6739d0374a4db6d2d9f30
nattanjunior <fnatanieljunior@gmail.com>
2024-11-18 17:00:22 -0300

    criada a funcionalidade de adicionar produtos ao carrinho

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
+import { useDataStore } from "./SelectedProducts";
 
 interface ParameterProps {
+  _id: string,
   name: string,
   price: number,
   description: string,
   picture: string
 }
 
-export default function Product({ name, price, description, picture }: ParameterProps) {
+export default function Product({ _id, name, price, description, picture }: ParameterProps) {
+  const setSelectedProducts = useDataStore(state => state.setSelectedProducts)
+
+  function addProduct() {
+    setSelectedProducts([_id])
+  }
+
   return (
     <div className="w-64 ">
       <div className="bg-blue-100 p-5 rounded-xl">
@@ -22,7 +30,7 @@ export default function Product({ name, price, description, picture }: Parameter
       </div>
       <div className="flex mt-1">
         <p className="text-2xl font-bold grow">${price}</p>
-        <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
+        <button onClick={addProduct} className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
       </div>
     </div>
   )
